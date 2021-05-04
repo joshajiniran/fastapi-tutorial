d97ee4464bfaf31ba6885eecaadb1515cc610fd5
Josh Ajiniran <josuajiniran@gmail.com>
2021-05-04 16:27:24 +0100

    feature: disable calling glo lines

--- phone_checker.py
+++ phone_checker.py
@@ -1,5 +1,14 @@
+# function
 def check_phone_number_correct(phone: str):
     if len(phone) < 11 or len(phone) > 11:
         return False
 
     return True
+
+
+# subroutine
+def check_can_call_phone_number(phone: str):
+    if phone.startswith('0805') or phone.startswith('0815') or phone.startswith('0807') or phone.startswith('0817'):
+        print("Calls to this line are not allowed, please try other number!")
+    else:
+        print("Calling {}...".format(phone))
--- tutorial.py
+++ tutorial.py
@@ -4,6 +4,6 @@ print("Welcome to our call center\n")
 callee = input("Enter phone number to call: ")
 
 if phone_checker.check_phone_number_correct(callee) is True:
-    print("Calling {}...".format(callee))
+    phone_checker.check_can_call_phone_number(callee)
 else:
     print("Please check if phone number is correct")
